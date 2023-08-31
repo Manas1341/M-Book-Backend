23db88b411b1840881bef51ecbb49c1dbc4491b6
Manas1341 <manasparatane005@gmail.com>
2023-09-01 00:39:19 +0530

    changes

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-const mongoURI = "mongodb://127.0.0.1:27017/inotebook?directConnection=true"
+const mongoURI = process.env.DATABASE
 
 const connectToMongo =()=>{
     mongoose.connect(mongoURI,{
